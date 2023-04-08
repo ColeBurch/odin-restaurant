adc8386b3f4732e7e1d291d8568b0a443b5a4717
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-04-08 10:48:46 -0600

    Create and link menuScreen.js

---
+++ menuScreen.bundle.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunkodin_restaurant"] = self["webpackChunkodin_restaurant"] || []).push([["menuScreen"],{
+
+/***/ "./src/menuScreen.js":
+/*!***************************!*\
+  !*** ./src/menuScreen.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ menuScreen)
+/* harmony export */ });
+function menuScreen() {
+  console.log("I am from menuScreen.js");
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/menuScreen.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVudVNjcmVlbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFlO0FBQ2Y7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tcmVzdGF1cmFudC8uL3NyYy9tZW51U2NyZWVuLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIG1lbnVTY3JlZW4oKSB7XG4gIGNvbnNvbGUubG9nKFwiSSBhbSBmcm9tIG1lbnVTY3JlZW4uanNcIik7XG59XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
